3f7856e771e92e937e47c1b121e6e737a2d15530
niko <niko@niko.io>
2022-05-18 17:56:34 +0200

    Updated the pouchdb import,
    we discovered the issue is in the idb module, as memory works fine.

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,26 @@
-import { createRxDatabase } from 'rxdb';
+import { createRxDatabase, addRxPlugin } from 'rxdb';
 
-import { getRxStoragePouch } from 'rxdb/plugins/pouchdb';
+// because we use the PouchDB RxStorage, we have to add the indexeddb adapter first.
+import { getRxStoragePouch, addPouchPlugin } from 'rxdb/plugins/pouchdb';
+import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
+import * as MemoryAdapter from 'pouchdb-adapter-memory';
+import * as IdbAdapter from 'pouchdb-adapter-idb';
 
+// import * as aa from 'pouchdb-adapter-idb'
 
-export const db = createRxDatabase({
-  name: 'heroesdb',
-  storage: getRxStoragePouch('idb')
+// addRxPlugin(RxDBDevModePlugin);
+
+addPouchPlugin(MemoryAdapter);
+addPouchPlugin(IdbAdapter);
+
+export const db = () => createRxDatabase({
+  name: 'ourdb',                   // <- name
+  storage: getRxStoragePouch('memory'),  // <- RxStorage
+  //  password: 'myPassword',             // <- password (optional)
+  // multiInstance: true,                // <- multiInstance (optional, default: true)
+
+  // eventReduce: true,                   // <- eventReduce (optional, default: true)
+  cleanupPolicy: {}                   // <- custom cleanup policy (optional)
 });
 
+db().then(console.log)
