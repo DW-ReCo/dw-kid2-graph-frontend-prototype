8599d5013c150b6757f405af1995498dfc660398
tlmn <mail@tlmn.io>
2022-05-19 09:42:33 +0200

    add prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,6 @@ module.exports = {
       version: "detect",
     },
   },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
+  extends: ["plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
   rules: {},
 };
