3cb00b179b54b7a277b3b84f944165a6207b1a2b
tlmn <mail@tlmn.io>
2022-05-19 09:44:52 +0200

    prettier

--- db/index.ts
+++ db/index.ts
@@ -1,26 +1,23 @@
-import * as rxdb from 'rxdb'
+import * as rxdb from "rxdb";
 
 // because we use the PouchDB RxStorage, we have to add the indexeddb adapter first.
 //import { getRxStoragePouch, addPouchPlugin } from 'rxdb/plugins/pouchdb';
-import * as pouchdb from 'rxdb/plugins/pouchdb';
+import * as pouchdb from "rxdb/plugins/pouchdb";
 
-import * as MemoryAdapter from 'pouchdb-adapter-memory';
-import * as IdbAdapter from 'pouchdb-adapter-idb';
+import * as MemoryAdapter from "pouchdb-adapter-memory";
+import * as IdbAdapter from "pouchdb-adapter-idb";
 
 // import * as aa from 'pouchdb-adapter-idb'
 
 // addRxPlugin(RxDBDevModePlugin);
 
-
 export const createDb = async () => {
-
   pouchdb.addPouchPlugin(MemoryAdapter);
   pouchdb.addPouchPlugin(IdbAdapter);
 
   return rxdb.createRxDatabase({
-    name: 'ourdb',                   // <- name
-    storage: pouchdb.getRxStoragePouch('idb'),  // <- RxStorage
-    cleanupPolicy: {}                   // <- custom cleanup policy (optional)
+    name: "ourdb", // <- name
+    storage: pouchdb.getRxStoragePouch("idb"), // <- RxStorage
+    cleanupPolicy: {}, // <- custom cleanup policy (optional)
   });
-}
-
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-import React from "react"
+import React from "react";
 
-import * as db from "../../db"
+import * as db from "../../db";
 
 const IndexPage = () => {
+  if (typeof window !== "undefined") {
+    // client-side-only code
+    db.createDb().then(console.log);
+  }
 
-    if (typeof window !== 'undefined') {
-        // client-side-only code
-        db.createDb().then(console.log)
-    }
+  return <div>Hello from IndexPage</div>;
+};
 
-    return(
-    <div>Hello from IndexPage</div>
-)}
-
-export default IndexPage
+export default IndexPage;
