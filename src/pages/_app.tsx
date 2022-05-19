edc18dea016a5507f2c5633d07f630114bb89561
tlmn <mail@tlmn.io>
2022-05-19 16:09:55 +0200

    use subscribeQuery()

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Provider } from "rxdb-hooks";
 import { initialize } from "../../db";
-import { listenToCharacters } from "../../utils/subscribeQuery"
+import { subscribeQuery } from "../../utils/subscribeQuery"
 
 const App = ({ Component, pageProps }) => {
   const [db, setDb] = useState();
 
   useEffect(() => {
+    
     const initDB = async () => {
       const _db = await initialize();
       setDb(_db);
-      listenToCharacters(_db);
+      subscribeQuery(_db.characters.find().where({name: {$eq: "Niko"}}), () => {console.log("Name ist Niko")} );
 
     };
     if (window !== undefined) {
