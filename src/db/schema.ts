66a81f78e215e226bc35b6caa9dcb441c2d22770
niko <niko@niko.io>
2022-06-09 17:31:57 +0200

    add another field to query on for #19

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,5 +2,10 @@ const collectionName = "docs" // FIXME we need something better than this
 
 export const collectionSchema = {
   // TODO add document_type and _type to the schema see the types
-  [collectionName]: { schema: { version: 0, type: "object", primaryKey: "id", properties: { id: { type: "string" } } } }
+  [collectionName]: { schema: { version: 0,
+                                type: "object",
+                                primaryKey: "id",
+                                properties: { id: { type: "string", maxLength: 100 },
+                                              document_type: { type: "string", maxLength: 100 }},
+                                } }
 }
