63f8ac7391186a237f5a5482a6c47063ba04dc77
tlmn <mail@tlmn.io>
2022-06-15 14:28:58 +0200

    make full React component and remove unneeded prop

--- src/frontend/components/pageListItem/pageListItem.tsx
+++ src/frontend/components/pageListItem/pageListItem.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import * as dbTypes from "../../../db/types";
 
-const PageListItem = (dbL: dbTypes.LoadedDb, page: dbTypes.Page, open: () => void) => (
-  <div>
-    {page.title}
-    <button onClick={open}>Open</button>
-  </div>
-);
+const PageListItem = (props: { page: dbTypes.Page; open: () => void }) => {
+  const { page, open } = props;
+
+  return (
+    <div>
+      {page.title}
+      <button onClick={open}>Open</button>
+    </div>
+  );
+};
 
 export default PageListItem;
