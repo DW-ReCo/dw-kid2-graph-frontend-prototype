17446b6f34cc710a6eb4cb208162f0fa90109a78
tlmn <mail@tlmn.io>
2022-06-17 21:28:55 +0200

    add simple-import-sort (#43)

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,9 +13,24 @@ module.exports = {
     },
   },
   extends: ["plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  plugins: ["simple-import-sort"],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-namespace": "off",
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          ["^react", "^@?\\w"],
+          ["^(@|components)(/.*|$)"],
+          ["^\\u0000"],
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          ["^.+\\.?(css)$"],
+        ],
+      },
+    ],
+    "simple-import-sort/exports": "error",
   },
 };
