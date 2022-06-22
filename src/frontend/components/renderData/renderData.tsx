d471766200bf50282cbf7e2cd4ea68b7d7cebaa4
tlmn <mail@tlmn.io>
2022-06-22 10:19:59 +0200

    use syntax highlighting (#75)

--- src/frontend/components/renderData/renderData.tsx
+++ src/frontend/components/renderData/renderData.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import * as types from "./types";
+import { syntaxHighlight } from "@utils/index";
 
 const RenderData = ({ documents, title }: types.RenderDataPropTypes) => (
-  <>
+  <div className="flex-1 max-w-1/4">
     <h2>{title}</h2>
-    <ul>
+    <ul className="pt-4">
       {documents.map((document: object, index: number) => (
-        <li key={index}>
-          <pre>{JSON.stringify(document, null, 2)}</pre>
+        <li key={index} className="pb-2">
+          <pre
+            className="text-xs"
+            dangerouslySetInnerHTML={{ __html: syntaxHighlight(JSON.stringify(document, undefined, 4)) }}
+          />
         </li>
       ))}
     </ul>
-  </>
+  </div>
 );
 
 export default RenderData;
