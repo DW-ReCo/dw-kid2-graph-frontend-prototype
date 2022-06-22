f71fb6c763f40e13a61023adb5501abd6d32b894
tlmn <mail@tlmn.io>
2022-06-22 13:46:21 +0200

    add icons (#82)

--- src/frontend/components/pageListItem/pageListItem.tsx
+++ src/frontend/components/pageListItem/pageListItem.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import * as DatabaseTypes from "@db/types";
 
 import * as Queries from "@db/queries";
+import OpenIcon from "@frontend/assets/icons/open";
+import DeleteIcon from "@frontend/assets/icons/delete";
 
 const PageListItem = (props: { db: DatabaseTypes.LoadedDb; page: DatabaseTypes.Page; open: () => void }) => {
   const { page, open, db } = props;
@@ -13,11 +15,18 @@ const PageListItem = (props: { db: DatabaseTypes.LoadedDb; page: DatabaseTypes.P
   const remove = (db: DatabaseTypes.LoadedDb, id: string) => Queries.remove(db.instance, id);
 
   return (
-    <div>
-      <input onChange={(e) => updatePageTitle(e.target.value)} value={page.title} />
-      <button onClick={open}>Open</button>
-      <button onClick={() => remove(db, page.id)}>Remove</button>
-    </div>
+    <li className="inline-flex flex-nowrap">
+      <input onChange={(e) => updatePageTitle(e.target.value)} value={page.title} className="w-[9rem] p-1" />
+      <button onClick={open} className="text-black p-0 bg-transparent hover:bg-transparent hover:text-slate-500">
+        <OpenIcon />
+      </button>
+      <button
+        onClick={() => remove(db, page.id)}
+        className="text-black p-0 bg-transparent hover:bg-transparent hover:text-slate-500"
+      >
+        <DeleteIcon />
+      </button>
+    </li>
   );
 };
 
