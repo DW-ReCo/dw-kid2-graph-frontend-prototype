192e5d2b5776df0afcc453a636d45ee13883fd1b
tlmn <mail@tlmn.io>
2022-06-22 10:02:41 +0200

    style (#76)

--- src/frontend/pages/dev/panel.tsx
+++ src/frontend/pages/dev/panel.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import * as db from "@db/index";
 
 import { addTestingData } from "../../../db/testing_data";
@@ -7,9 +7,12 @@ import { first } from "lodash/fp";
 import useDbContext from "@frontend/hooks/contexts/useDbContext";
 
 import Link from "next/link";
+import clsx from "clsx";
 
 const DevPanel = () => {
   const { dbState: dbs } = useDbContext();
+
+  const [devPanelState, setDevPanelState] = useState(true);
 
   const firstDb = first(dbs);
 
@@ -18,30 +21,65 @@ const DevPanel = () => {
   const clearDbs = () => dbs.map((d) => db.clearDocs(d.instance));
 
   const addTestingDataDbs = () => dbs.map((d) => addTestingData(d.instance));
-  console.log(dbs);
+
+  const LINKS = [
+    { label: "Database view", href: "/db" },
+    { label: "Application prototype", href: "/app" },
+    { label: "Config editor", href: "/config" },
+  ];
 
   return (
-    <>
-      <Link href="/db">
-        <a>databases view | </a>
-      </Link>
-      <Link href="/app">
-        <a>application prototype | </a>
-      </Link>
-      <Link href="/config">
-        <a>config editor</a>
-      </Link>
-      <button onClick={clearDbs}>clear the documents (all dbs)</button>
-      <button onClick={addTestingDataDbs}>add testing data (all dbs)</button>
-      <>
-        Databases:
-        {dbs.map((d, index) => (
-          <Fragment key={index}>
-            <span>{d.name} |</span>
-          </Fragment>
-        ))}
-      </>
-    </>
+    <div className={clsx("flex-end bg-orange-100 min-h-screen p-2 ml-2", devPanelState ? "w-72" : "w-5")}>
+      <button
+        className={clsx("button-dev-panel-toggle", !devPanelState && "rotate-180")}
+        onClick={() => setDevPanelState(!devPanelState)}
+      >
+        ▶
+      </button>
+      <div>
+        {devPanelState && (
+          <>
+            <details open>
+              <summary>
+                <h2>🖥 Views</h2>
+              </summary>
+              <ul>
+                {LINKS.map(({ href, label }, index) => (
+                  <li key={index}>
+                    <Link href={href}>
+                      <a>{label}</a>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </details>
+            <details open>
+              <summary>
+                <h2>⚒ Database Actions</h2>
+              </summary>
+              <button className="button--small" onClick={clearDbs}>
+                clear the documents (all dbs)
+              </button>
+              <button className="button--small" onClick={addTestingDataDbs}>
+                add testing data (all dbs)
+              </button>
+            </details>
+            <details open>
+              <summary>
+                <h2>🚦 Status</h2>
+              </summary>
+              <h3>App</h3>
+              <h3>Databases</h3>
+              {dbs.map((d, index) => (
+                <Fragment key={index}>
+                  <span>{d.name} |</span>
+                </Fragment>
+              ))}
+            </details>
+          </>
+        )}
+      </div>
+    </div>
   );
 };
 
