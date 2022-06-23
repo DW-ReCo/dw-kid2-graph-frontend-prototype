89aeb034d6182a1d1c0ff8537f0b7e94798b4144
tlmn <mail@tlmn.io>
2022-06-23 12:39:50 +0200

    use sanitization (#75)

--- src/frontend/components/renderData/renderData.tsx
+++ src/frontend/components/renderData/renderData.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import * as types from "./types";
 import { syntaxHighlight } from "@utils/index";
+import DOMPurify from "dompurify";
 
-const RenderData = ({ documents, title }: types.RenderDataPropTypes) => (
-  <div className="flex-1 max-w-1/4">
-    <h2>{title}</h2>
-    <ul className="pt-4">
-      {documents.map((document: object, index: number) => (
-        <li key={index} className="pb-2">
-          <pre
-            className="text-xs"
-            dangerouslySetInnerHTML={{ __html: syntaxHighlight(JSON.stringify(document, undefined, 2)) }}
-          />
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const RenderData = ({ documents, title }: types.RenderDataPropTypes) => {
+  const documentSyntaxHighlighted = syntaxHighlight(JSON.stringify(document, undefined, 2));
+  const cleanMarkup = DOMPurify.sanitize(documentSyntaxHighlighted);
+
+  return (
+    <div className="flex-1 max-w-1/4">
+      <h2>{title}</h2>
+      <ul className="pt-4">
+        {documents.map((document: object, index: number) => (
+          <li key={index} className="pb-2">
+            <pre className="text-xs" dangerouslySetInnerHTML={{ __html: cleanMarkup }} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default RenderData;
