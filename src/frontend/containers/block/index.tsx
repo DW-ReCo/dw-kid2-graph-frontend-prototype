1a5774dfabde06256826bb8f41cfeb0928cabe03
tlmn <mail@tlmn.io>
2022-06-23 15:41:14 +0200

    render blocks in order (closes #58) (#86)
    * render blocks in order (#58)
    
    * implement moving blocks logic (#58)

--- src/frontend/containers/block/index.tsx
+++ src/frontend/containers/block/index.tsx
@@ -2,6 +2,8 @@ import * as Note from "./note";
 import * as YoutubeInput from "./youtubeLink";
 import * as DatabaseTypes from "@db/types";
 import React from "react";
+import { moveElementPosition } from "@frontend/utils";
+import * as Queries from "@db/queries";
 
 const NotFoundType = ({ block }: { block: DatabaseTypes.Block }) => <>Block type {block?.type} not found</>;
 
@@ -11,14 +13,28 @@ const BlockSwitch = ({ db, block }: { db: DatabaseTypes.LoadedDb; block: Databas
   block.type === "youtube_url_input"  ? <YoutubeInput.Component db={db} block={block} /> :
                                         <NotFoundType block={block} />;
 
-const Block = (props: { db: DatabaseTypes.LoadedDb; block: DatabaseTypes.Block }) => {
-  const { block, db } = props;
+const Block = (props: { db: DatabaseTypes.LoadedDb; block: DatabaseTypes.Block; page: DatabaseTypes.Page }) => {
+  const { block, db, page } = props;
+
+  const blockIndex = page.blocks.indexOf(block.id);
+
+  const blocksLength = page.blocks.length;
+
+  const handleIndexUpdate = (blockId: string, indexChange: number) => {
+    const fromIndex = page.blocks.indexOf(blockId);
+    const newPageBlocks = moveElementPosition(page.blocks, fromIndex, fromIndex + indexChange);
+    Queries.mergePage(db.instance, { id: page.id, blocks: newPageBlocks });
+  };
 
   return (
     <div className="block">
       <span className="meta-tag">{block.id}</span>
       <span className="meta-tag">{block.document_type}</span>
       <span className="meta-tag">{block.type}</span>
+      {blockIndex !== 0 && blocksLength !== 1 && <button onClick={() => handleIndexUpdate(block.id, -1)}>up</button>}
+      {blocksLength !== 1 && blocksLength - 1 !== blockIndex && (
+        <button onClick={() => handleIndexUpdate(block.id, 1)}>down</button>
+      )}
       <BlockSwitch db={db} block={block} />
     </div>
   );
