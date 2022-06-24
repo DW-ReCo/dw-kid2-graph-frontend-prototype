c53c8673c23288c0ac578528b84f5bb7fa5e05ca
tlmn <mail@tlmn.io>
2022-06-24 13:00:28 +0200

    add messages (#78)

--- src/utils/statusMessages.ts
+++ src/utils/statusMessages.ts
@@ -1,6 +1,6 @@
 export const STATUS_MESSAGES = [
-  { messageCode: "DB_LOADED", message: "Database loaded" },
-  { messageCode: "CONFIG_LOADED", message: "Configuration loaded" },
+  { messageCode: "DB_OK", message: "Database loaded" },
+  { messageCode: "CONFIG_OK", message: "Configuration loaded" },
   { messageCode: "APP_INITIAL", message: "App initial state" },
   { messageCode: "CONFIG_INITIAL", message: "Config initial state" },
   { messageCode: "DB_INITIAL", message: "Database initial state" },
