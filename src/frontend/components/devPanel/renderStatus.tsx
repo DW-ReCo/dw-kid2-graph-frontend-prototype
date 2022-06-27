675d765bf8c4dc4844d3d95cfc34144db4ae2171
tlmn <mail@tlmn.io>
2022-06-27 15:00:15 +0200

    update props (#95)

--- src/frontend/components/devPanel/renderStatus.tsx
+++ src/frontend/components/devPanel/renderStatus.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import * as Types from "@data-types/index";
 import { getStatusIcon, getStatusMessage } from "@utils/index";
+import * as Types from "@data-types/index";
 
-const RenderStatus = (status: Types.status) => (
+const RenderStatus = ({ status }: { status: Types.status }) => (
   <div className="block">
     <span className="mr-2">{getStatusIcon(status.diagnostic)}</span>
     <span>{getStatusMessage(status.message)}</span>
