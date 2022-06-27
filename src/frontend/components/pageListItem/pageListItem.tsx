7696a1083cf360f1b5bc9722c7f3c291ce2521df
tlmn <mail@tlmn.io>
2022-06-27 18:11:35 +0200

    make page title editable and style (#97)

--- src/frontend/components/pageListItem/pageListItem.tsx
+++ src/frontend/components/pageListItem/pageListItem.tsx
@@ -1,28 +1,38 @@
 import React from "react";
 import * as DatabaseTypes from "@data-types/index";
+import clsx from "clsx";
 
 import * as Queries from "@db/queries";
-import OpenIcon from "@frontend/assets/icons/open";
 import DeleteIcon from "@frontend/assets/icons/delete";
+import useAppContext from "@frontend/hooks/contexts/useAppContext";
 
 const PageListItem = (props: { db: DatabaseTypes.LoadedDb; page: DatabaseTypes.Page; open: () => void }) => {
   const { page, open, db } = props;
 
-  const updatePageTitle = (newTitle: string) => {
-    Queries.mergePage(db.instance, { document__id: page.document__id, page__title: newTitle });
-  };
+  const {
+    // @ts-ignore
+    appState: {
+      // @ts-ignore
+      app: { activePage },
+    },
+  } = useAppContext();
 
   const remove = (db: DatabaseTypes.LoadedDb, id: string) => Queries.remove(db.instance, id);
 
   return (
     <li className="inline-flex flex-nowrap">
-      <input onChange={(e) => updatePageTitle(e.target.value)} value={page.page__title} className="w-[9rem] p-1" />
-      <button onClick={open} className="text-black p-0 bg-transparent hover:bg-transparent hover:text-slate-500">
-        <OpenIcon />
+      <button
+        onClick={open}
+        className={clsx(
+          "w-[9rem] p-1 text-black m-0 text-left",
+          activePage === page.document__id ? "bg-teal-200" : "bg-transparent",
+        )}
+      >
+        {page.page__title}
       </button>
       <button
         onClick={() => remove(db, page.document__id)}
-        className="text-black p-0 bg-transparent hover:bg-transparent hover:text-slate-500"
+        className="text-black p-0 bg-transparent hover:bg-transparent hover:text-slate-500  m-1"
       >
         <DeleteIcon />
       </button>
