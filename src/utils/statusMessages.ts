f68a95ed6571716bd7e61a85b7f19febe9072b7f
tlmn <mail@tlmn.io>
2022-06-27 15:00:58 +0200

    add messages (#95)

--- src/utils/statusMessages.ts
+++ src/utils/statusMessages.ts
@@ -1,7 +1,12 @@
 export const STATUS_MESSAGES = [
   { messageCode: "APP_OK", message: "App loaded" },
-  { messageCode: "DB_OK", message: "Database loaded" },
+  { messageCode: "APP_ERROR", message: "App encountered error" },
+  { messageCode: "DB_LOADED", message: "Database loaded" },
+  { messageCode: "DB_NO_DB", message: "No Database loaded" },
+  { messageCode: "DB_UNKNOWN", message: "Database status unknown" },
+  { messageCode: "CONFIG_NOT_FOUND", message: "Configuration not found" },
   { messageCode: "CONFIG_OK", message: "Configuration loaded" },
+  { messageCode: "CONFIG_NO_DBS", message: "No database configuration found" },
   { messageCode: "APP_INITIAL", message: "App initial state" },
   { messageCode: "CONFIG_INITIAL", message: "Config initial state" },
   { messageCode: "DB_INITIAL", message: "Database initial state" },
