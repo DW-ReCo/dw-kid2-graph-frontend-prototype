fa586b5f16ca2cd51a9c7f57183be7f39c1dae7e
tlmn <mail@tlmn.io>
2022-06-28 11:47:58 +0200

    style and fix delete function (#97)

--- src/frontend/containers/block/index.tsx
+++ src/frontend/containers/block/index.tsx
@@ -6,6 +6,7 @@ import { moveElementPosition } from "@frontend/utils";
 import * as Queries from "@db/queries";
 import ArrowIcon from "@frontend/assets/icons/arrow";
 import DeleteIcon from "@frontend/assets/icons/delete";
+import { without } from "lodash";
 
 const NotFoundType = ({ block }: { block: Types.Block }) => <>Block type {block.block__type} not found</>;
 
@@ -29,6 +30,10 @@ const Block = (props: { db: Types.LoadedDb; block: Types.Block; page: Types.Page
   };
 
   const handleDelete = (blockId: string): void => {
+    Queries.mergePage(db.instance, {
+      document__id: page.document__id,
+      page__blocks: without(page.page__blocks, blockId),
+    });
     Queries.remove(db.instance, blockId);
   };
 
@@ -36,22 +41,28 @@ const Block = (props: { db: Types.LoadedDb; block: Types.Block; page: Types.Page
 
   return (
     <div className="app-block">
-      <span className="meta-tag">{id}</span>
-      <span className="meta-tag">{documentType}</span>
-      <span className="meta-tag">{blockType}</span>
-      {blockIndex !== 0 && blocksLength !== 1 && (
-        <button onClick={() => handleIndexUpdate(id, -1)}>
-          <ArrowIcon />
-        </button>
-      )}
-      {blocksLength !== 1 && blocksLength - 1 !== blockIndex && (
-        <button onClick={() => handleIndexUpdate(id, 1)}>
-          <ArrowIcon className="rotate-180" />
-        </button>
-      )}
-      <button onClick={() => handleDelete(id)}>
-        <DeleteIcon />
-      </button>
+      <div className="flex flex-row">
+        <div className="flex-1">
+          <span className="meta-tag">{id}</span>
+          <span className="meta-tag">{documentType}</span>
+          <span className="meta-tag">{blockType}</span>
+        </div>
+        <div>
+          {blockIndex !== 0 && blocksLength !== 1 && (
+            <button className="bg-transparent hover:bg-transparent text-[]" onClick={() => handleIndexUpdate(id, -1)}>
+              <ArrowIcon />
+            </button>
+          )}
+          {blocksLength !== 1 && blocksLength - 1 !== blockIndex && (
+            <button className="bg-transparent hover:bg-transparent" onClick={() => handleIndexUpdate(id, 1)}>
+              <ArrowIcon className="rotate-180" />
+            </button>
+          )}
+          <button className="bg-transparent hover:bg-transparent" onClick={() => handleDelete(id)}>
+            <DeleteIcon />
+          </button>
+        </div>
+      </div>
       <BlockSwitch db={db} block={block} />
     </div>
   );
