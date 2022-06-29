4c64726607fdfc8342638b2e430dd7f8c8d70819
tlmn <mail@tlmn.io>
2022-06-29 15:17:09 +0200

    fix import (#103)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 export * as Block from "./block";
 export * as Config from "./config";
-export * as Context from "./contexts";
+export * as Context from "./context";
 export * as Data from "./data-node";
 export * as Database from "./database";
 export * as Document from "./document";
