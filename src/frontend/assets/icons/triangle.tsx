0cd9a3077a3a9865a390b3469d5257ffcd8f0851
tlmn <mail@tlmn.io>
2022-06-30 16:48:48 +0200

    add triangle icon (#115)

---
+++ src/frontend/assets/icons/triangle.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TriangleIcon = ({ width = 10 }: { width?: number }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={width} viewBox="0 0 123.959 123.959">
+    <path
+      fill="currentColor"
+      d="M85.742 1.779l-56 56c-2.3 2.3-2.3 6.1 0 8.401l56 56c3.801 3.8 10.2 1.1 10.2-4.2v-112c0-5.301-6.399-8.001-10.2-4.201z"
+    ></path>
+  </svg>
+);
+
+export default TriangleIcon;
