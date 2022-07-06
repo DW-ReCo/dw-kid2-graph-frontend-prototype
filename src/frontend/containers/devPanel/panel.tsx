5d0601d282c585ee08d1632093bf98631932b5ef
tlmn <mail@tlmn.io>
2022-07-06 07:26:32 +0200

    use triangle icon (#115)

--- src/frontend/containers/devPanel/panel.tsx
+++ src/frontend/containers/devPanel/panel.tsx
@@ -10,6 +10,7 @@ import * as Types from "@data-types/index";
 import * as Database from "@database/index";
 import { addTestingData } from "@database/testing_data";
 
+import TriangleIcon from "@frontend/assets/icons/triangle";
 import RenderStatus from "@frontend/components/devPanel/renderStatus";
 import useAppContext from "@frontend/hooks/contexts/useAppContext";
 import useConfigContext from "@frontend/hooks/contexts/useConfigContext";
@@ -73,88 +74,89 @@ const DevPanel = () => {
 
   return (
     <>
-      <div className="flex-end">
+      <div
+        className={clsx(
+          "bg-orange-100 flex-end min-h-screen p-2 ml-2 sticky max-h-screen top-0 right-0 overflow-y-scroll",
+          showDevPanel ? "w-72" : "w-5",
+        )}
+      >
         <button
-          className={clsx("button-dev-panel-toggle z-10", !showDevPanel && "rotate-180")}
+          className={clsx(
+            "button-dev-panel-toggle text-white flex justify-center items-center",
+            showDevPanel && "rotate-180",
+          )}
           onClick={() => setAppState((prev) => ({ ...prev, app: { ...prev.app, showDevPanel: !showDevPanel } }))}
         >
-          ▶
+          <TriangleIcon />
         </button>
-        <div
-          className={clsx(
-            "bg-orange-100 min-h-screen p-2 ml-2 sticky max-h-screen top-0 right-0 overflow-y-scroll",
-            showDevPanel ? "w-72" : "w-5",
-          )}
-        >
-          <div className="">
-            {showDevPanel && (
-              <>
-                <details open>
-                  <summary>
-                    <h2>🖥 Views</h2>
-                  </summary>
-                  <ul>
-                    {LINKS.map(({ href, label }, index) => (
-                      <li key={index}>
-                        <Link href={href}>
-                          <a>{label}</a>
-                        </Link>
-                      </li>
+        <div className="">
+          {showDevPanel && (
+            <>
+              <details open>
+                <summary>
+                  <h2>🖥 Views</h2>
+                </summary>
+                <ul>
+                  {LINKS.map(({ href, label }, index) => (
+                    <li key={index}>
+                      <Link href={href}>
+                        <a>{label}</a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </details>
+              <details open>
+                <summary>
+                  <h2>⚒ Database Actions</h2>
+                </summary>
+                <button className="button--small" onClick={clearDbs}>
+                  clear the documents (all dbs)
+                </button>
+                <button className="button--small" onClick={addTestingDataDbs}>
+                  add testing data (all dbs)
+                </button>
+              </details>
+              <details open>
+                <summary>
+                  <h2>🚦 Status</h2>
+                </summary>
+                <h3>App</h3>
+                <RenderStatus status={status.app.status} />
+                <h3>Config</h3>
+                <RenderStatus status={status.config.status} />
+                <h3>Databases</h3>
+                <RenderStatus status={status.db.status} />
+
+                <div className="flex flex-col my-2">
+                  <label className="uppercase text-xs font-bold text-slate-600">active Database</label>
+                  <span className="pl-2">{activeDatabase}</span>
+                </div>
+                <div className="flex flex-col mb-2">
+                  <label className="uppercase text-xs font-bold text-slate-600">active Page</label>
+                  <span className="pl-2">{activePage}</span>
+                </div>
+                <div className="flex flex-col mb-2">
+                  <label className="uppercase text-xs font-bold text-slate-600">loaded Databases</label>
+                  <ul className="pl-2">
+                    {dbs.map((d, index) => (
+                      <li key={index}>{d.name}</li>
                     ))}
                   </ul>
-                </details>
-                <details open>
-                  <summary>
-                    <h2>⚒ Database Actions</h2>
-                  </summary>
-                  <button className="button--small" onClick={clearDbs}>
-                    clear the documents (all dbs)
-                  </button>
-                  <button className="button--small" onClick={addTestingDataDbs}>
-                    add testing data (all dbs)
-                  </button>
-                </details>
-                <details open>
-                  <summary>
-                    <h2>🚦 Status</h2>
-                  </summary>
-                  <h3>App</h3>
-                  <RenderStatus status={status.app.status} />
-                  <h3>Config</h3>
-                  <RenderStatus status={status.config.status} />
-                  <h3>Databases</h3>
-                  <RenderStatus status={status.db.status} />
-
-                  <div className="flex flex-col my-2">
-                    <label className="uppercase text-xs font-bold text-slate-600">active Database</label>
-                    <span className="pl-2">{activeDatabase}</span>
+                </div>
+              </details>
+              <details open>
+                {dbs.map((d) => (
+                  <div key={d.name}>
+                    <label className="uppercase text-xs font-bold text-slate-600 my-2">{d.name}</label>
+                    {services.map((s) => (
+                      <ServiceStatus key={s.name} db={d} config={configState} service={s} />
+                    ))}
                   </div>
-                  <div className="flex flex-col mb-2">
-                    <label className="uppercase text-xs font-bold text-slate-600">active Page</label>
-                    <span className="pl-2">{activePage}</span>
-                  </div>
-                  <div className="flex flex-col mb-2">
-                    <label className="uppercase text-xs font-bold text-slate-600">loaded Databases</label>
-                    <ul className="pl-2">
-                      {dbs.map((d, index) => (
-                        <li key={index}>{d.name}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </details>
-                <details open>
-                  {dbs.map((d) => (
-                    <div key={d.name}>
-                      <label className="uppercase text-xs font-bold text-slate-600 my-2">{d.name}</label>
-                      {services.map((s) => (
-                        <ServiceStatus key={s.name} db={d} config={configState} service={s} />
-                      ))}
-                    </div>
-                  ))}
-                </details>
-              </>
-            )}
-          </div>
+                ))}
+              </details>
+            </>
+          )}
         </div>
       </div>
     </>
